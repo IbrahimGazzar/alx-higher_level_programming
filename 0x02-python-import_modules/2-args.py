e50d982ef79cb56e59d9ef15428bd1ba591b06d0
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-09-07 08:15:19 -0700

    Prevent execution on import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-argc = len(argv) - 1
-print(f"{argc} argument", end="")
-if argc != 1:
-    print("s", end="")
-if argc == 0:
-    print(".")
-else:
-    print(":")
-for i in range(1, argc + 1):
-    print(f"{i}: {argv[i]}")
+    argc = len(argv) - 1
+    print(f"{argc} argument", end="")
+    if argc != 1:
+        print("s", end="")
+    if argc == 0:
+        print(".")
+    else:
+        print(":")
+    for i in range(1, argc + 1):
+        print(f"{i}: {argv[i]}")
