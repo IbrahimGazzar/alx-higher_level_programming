5d00922941eee29ef77f8a846f236aff856924d8
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-09-25 08:04:03 -0700

    Add condition for empty list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    i = 0
     for i in range(0, x):
         try:
             print(my_list[i], end="")
@@ -7,4 +8,6 @@ def safe_print_list(my_list=[], x=0):
             i -= 1
             break
     print("")
+    if i <= 0:
+        return 0
     return i + 1
