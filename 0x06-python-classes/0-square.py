18a1f90658514f7453f933975bb0e158d78e4deb
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-09-26 09:15:57 -0700

    Is this documentation good enough?

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""0-Square
+   This file demonstrates the declaration of a pythong class
+"""
+
 class Square:
     """This is an empty python class"""
     pass
