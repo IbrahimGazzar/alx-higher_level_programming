30b49f81a20861be8385ec1861948c027b66098e
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-09-30 09:42:53 -0700

    Task 3: Add the complete functional function

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -16,4 +16,9 @@ def say_my_name(first_name, last_name=""):
                 in case no second input is provided, it's replaced
                 with an empty string
     """
-    
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print(f"My name is {first_name} {last_name}")
