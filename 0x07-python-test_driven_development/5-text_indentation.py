31a92a6e5f67baec44e67ec3a3efbe29ef26bca1
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-09-30 12:24:15 -0700

    Task 5: Create an empty prototype

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    This module contains the function to be tested for task 5
+"""
+
+
+def text_indentation(text):
+    """
+        This function takes a string and indents it in a
+        specific format. Printing a new empty line after one
+        of the following characters: '.', '?', ':'
+
+        Args:
+            @text: the string to be formatted
+    """
+    
