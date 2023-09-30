2368c5dab1c3d6cfe50d85da2cb5d1b0797611e4
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-09-30 13:03:37 -0700

    Test 5: fix errors in test suite, and add the text indentation function

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,4 +13,13 @@ def text_indentation(text):
         Args:
             @text: the string to be formatted
     """
-    
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    for i in range(0, len(text)):
+        if i > 0 and text[i - 1] in ['.', '?', ':'] and text[i] == ' ':
+            pass
+        else:
+            print(f"{text[i]}", end="")
+        if text[i] in ['.', '?', ':']:
+            print("\n")
