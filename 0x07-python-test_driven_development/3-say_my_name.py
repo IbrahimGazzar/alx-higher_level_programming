f4cd9e8e183c727e0ea8f4968f3ff1d1be70ab2f
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-09-30 09:21:54 -0700

    Task 2: Set function prototype with basic documentation

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    This module contains the function needed for task 2
+    of this python project
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+        This function prints the given inputs in the format:
+            My name is <first name> <last name>
+
+        Args:
+            @first_name: string to be used as the first name
+            @last_name: string to be used as the last name
+                in case no second input is provided, it's replaced
+                with an empty string
+    """
+    
