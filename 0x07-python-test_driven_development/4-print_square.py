98277185d9ce3ab0b66c2fe82a600c9069cd3e2c
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-09-30 11:37:02 -0700

    Task 4: Create an empty prototype

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+   This module contains the function to be tested for task 2
+"""
+
+
+def print_square(size):
+    """
+        This function prints a square, using hashes
+
+        Args:
+            @size: side length of the square to be printed
+    """
+    
