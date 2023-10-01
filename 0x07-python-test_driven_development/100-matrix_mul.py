45efa28eb9cdbeb0046293777d4453de22a678a4
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-10-01 07:55:37 -0700

    Task 7: Create empty prototype

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    This module contains the function required for the
+    first optional task of the project
+"""
+
+
+def matrix_mul(m_a, m_b):
+    """
+        This function performs matrix multiplication
+        And returns the resulting matrix
+
+        Args:
+            @m_a: first matrix
+            @m_b: second matrix
+    """
+    
