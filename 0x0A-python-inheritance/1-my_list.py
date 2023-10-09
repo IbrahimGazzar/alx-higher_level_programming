04c0a3c4005adc9f33bbfbaf6aca9641b4a6ff63
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-10-09 13:53:14 -0700

    Create a class that improves list, and add a test suite for it

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    This module contains a class that
+    inherits the common object of type list
+"""
+
+class MyList(list):
+    """
+        A modified (better!) version of list
+    """
+    def print_sorted(self):
+        """
+            prints the list in a sorted fashion
+        """
+        list_sorted = self[:]
+        list_sorted.sort()
+        print(list_sorted)
+        
