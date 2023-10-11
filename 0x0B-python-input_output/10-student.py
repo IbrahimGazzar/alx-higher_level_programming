c3fea89650b8874be328dc73193c47af6399a7c5
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-10-11 07:05:49 -0700

    Add the ability to handpick which attributes are converted in our Student class

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+    This module contains the class Student
+"""
+
+
+class Student:
+    """
+        Simple class representing student information
+
+        Attributes:
+            first_name (str): student's fist name
+            last_name (str): student's last name
+            age (int): student's age
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+            Class object initializer
+
+            Args:
+                first_name (str): item to set to first name att
+                last_name (str): item to set to last name att
+                age (int): value to set to age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+            retrieves a dictionary representation of a Student
+
+            Args:
+                attrs (list): list of attribute to be returned
+        """
+        if attrs:
+            json = {}
+            for i in attrs:
+                if i in self.__dict__.keys():
+                    json[i] = self.__dict__[i]
+            return json
+        return self.__dict__
