71d4a8f4797c2d7e735de51bf64facf4dcde2d9c
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-10-12 14:44:13 -0700

    Add validation for the rectangle's attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -28,11 +28,12 @@ class Rectangle(Base):
                 y (int): y position
                 id (int): optional custom id
         """
+        
         Base.__init__(self, id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -40,6 +41,10 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -48,6 +53,10 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -56,6 +65,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -64,5 +77,9 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
 
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -16,8 +16,7 @@ class TestRectangle(unittest.TestCase):
         """
         self.r1 = Rectangle(10, 5)
         self.r2 = Rectangle(9, 8, 3)
-        self.r3 = Rectangle(3, 10, 1, 9)
-        self.r4 = Rectangle(-1, -1, 0, -10, 34)
+        self.r3 = Rectangle(3, 10, 1, 9, -1)
 
     def test_create_rect(self):
         """
@@ -28,6 +27,19 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(f"{self.r2.width}, {self.r2.height}, {self.r2.x}, {self.r2.y}, {self.r2.id}", \
                          "9, 8, 3, 0, 5")
         self.assertEqual(f"{self.r3.width}, {self.r3.height}, {self.r3.x}, {self.r3.y}, {self.r3.id}", \
-                         "3, 10, 1, 9, 6")
-        self.assertEqual(f"{self.r4.width}, {self.r4.height}, {self.r4.x}, {self.r4.y}, {self.r4.id}", \
-                         "-1, -1, 0, -10, 34")
+                         "3, 10, 1, 9, -1")
+
+    def test_rect_valid(self):
+        """
+            Tests the validity of rectangle attrs
+        """
+        self.assertRaises(TypeError, Rectangle)
+        self.assertRaises(TypeError, Rectangle, 2)
+        self.assertRaises(TypeError, Rectangle, x=10, y=10)
+        self.assertRaises(TypeError, Rectangle, "Hello", "World")
+        self.assertRaises(TypeError, Rectangle, 9, 10, "!")
+        self.assertRaises(ValueError, Rectangle, -1, 20)
+        self.assertRaises(ValueError, Rectangle, 20, -1)
+        self.assertRaises(ValueError, Rectangle, -1, "W")
+        self.assertRaises(ValueError, Rectangle, 20, 10, 90, -1)
+        
