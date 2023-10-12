2b68afcefa6c45cd1554cbb8f01ec5bd49606041
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-10-12 09:21:18 -0700

    Add the Base class and a test suite for it

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    This module contains the base class that
+    all others will inherit from
+"""
+
+
+class Base:
+    """
+        A basic class that handles ids
+
+        Attributes:
+            __nb_objects (int): number of objects
+                created in this class
+            id (int): individual object's id
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+            Intializing function for Base class
+
+            Args:
+                id (int): id of object
+        """
+        if id:
+            self.id = id
+        else:
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    Unittest folder for the class Base
+"""
+import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    """
+        Unittest class used for testing the
+        base class
+    """
+    def setUp(self):
+        """
+            Set up function for all the needed tests
+        """
+        self.b1 = Base()
+        self.b2 = Base()
+        self.b3 = Base(3)
+        self.b4 = Base()
+
+    def test_base_id(self):
+        """
+            tests the values of base ids
+        """
+        self.assertEqual(self.b1.id, 1)
+        self.assertEqual(self.b2.id, 2)
+        self.assertEqual(self.b3.id, 3)
+        self.assertEqual(self.b4.id, 3)
