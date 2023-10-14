500e0409f630ce21ffb371da298831db46406ecc
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-10-14 09:54:28 -0700

    Create a function that exports objects into json files

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,6 +30,24 @@ class Base:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
 
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+            takes a list of objects and saves
+            their JSON representation to a file
+
+            Args:
+                list_objs (list): list of objects
+                    to be saved
+        """
+        filename = str(cls.__name__) + ".json"
+        list_json = []
+        if list_objs is not None:
+            for i in list_objs:
+                list_json.append(dict(sorted(i.to_dictionary().items())))
+        with open(filename, 'w', encoding="utf-8") as f:
+            json.dump(list_json, f)
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """
@@ -37,7 +55,8 @@ class Base:
             list of dictionaries
 
             Args:
-                list_dictionaries: list of dictionaries
+                list_dictionaries (list): list of
+                    dictionaries to be converted
         """
         if not list_dictionaries:
             return "[]"
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -23,6 +23,8 @@ class TestBase(unittest.TestCase):
         self.b4 = Base()
         self.b5 = Base(-9)
         self.b6 = Base(0)
+        self.r1 = Rectangle(2, 2, 2, 2, 2)
+        self.s1 = Square(10, 2, 3, 0)
 
     def test_base_id(self):
         """
@@ -39,10 +41,8 @@ class TestBase(unittest.TestCase):
         """
             tests Base's to_json function
         """
-        r1 = Rectangle(2, 2, 2, 2, 2)
-        s1 = Square(10, 2, 3, 0)
-        r1_dict = dict(sorted(r1.to_dictionary().items()))
-        s1_dict = dict(sorted(s1.to_dictionary().items()))
+        r1_dict = dict(sorted(self.r1.to_dictionary().items()))
+        s1_dict = dict(sorted(self.s1.to_dictionary().items()))
 
         self.assertEqual(self.b1.to_json_string([r1_dict]), "[{\"height\": 2"
                          + ", \"id\": 2, \"width\": 2, \"x\": 2, \"y\": 2}]")
@@ -52,6 +52,26 @@ class TestBase(unittest.TestCase):
                          + "height\": 2, \"id\": 2, \"width\": 2, \"x\": 2"
                          + ", \"y\": 2}, {\"id\": 0, \"size\": 10, "
                          + "\"x\": 2, \"y\": 3}]")
+        self.assertEqual(self.b1.to_json_string([]), "[]")
+
+    def test_base_jfile(self):
+        """
+            Tests Base's JSON to file function
+        """
+        s2 = Square(4, 4, 4, 4)
+        Rectangle.save_to_file([self.r1])
+        Square.save_to_file([self.s1, s2])
+        Base.save_to_file([])
+
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(f.read(), "[{\"height\": 2, \"id\": 2, "
+                             + "\"width\": 2, \"x\": 2, \"y\": 2}]")
+        with open("Square.json", "r") as f:
+            self.assertEqual(f.read(), "[{\"id\": 0, \"size\": 10, "
+                             + "\"x\": 2, \"y\": 3}, {\"id\": 4, "
+                             + "\"size\": 4, \"x\": 4, \"y\": 4}]")
+        with open("Base.json", "r") as f:
+            self.assertEqual(f.read(), "[]")
 
     def TearDown(self):
         """
@@ -63,3 +83,5 @@ class TestBase(unittest.TestCase):
         del self.b4
         del self.b5
         del self.b6
+        del self.r1
+        del self.s1
