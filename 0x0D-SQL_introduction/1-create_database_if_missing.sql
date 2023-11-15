31a0c36165a6fee37dc70ee3993b7ce5abf72c7b
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-11-14 18:30:47 -0800

    Fix the SQL query to check if the database exists before creating it

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- script that creates the database hbtn_0c_0
--- creates the Database hbtn_0c_0
-CREATE DATABASE hbtn_0c_0;
+-- creates the Database hbtn_0c_0 if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
