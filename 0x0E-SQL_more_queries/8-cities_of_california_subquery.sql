eb76a1b4675236d40bd9fef8dfe557d2d17d1aad
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-11-16 06:27:13 -0800

    Create a script that performs a subquery spanning across two tables

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,3 @@
 -- A script that lists all the cities of California that can be found in the database hbtn_0d_usa.
 -- Select the state id of California with a subquery
-SELECT id, name FROM cities
-       WHERE state_id = (SELECT id from states WHERE name = 'California')
-       ORDER BY id;
+SELECT id, name FROM cities WHERE state_id = (SELECT id from states WHERE name = 'California') ORDER BY id;
