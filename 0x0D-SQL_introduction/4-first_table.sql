14ce1b2fef151cf0548a1886cd10f1c68e28bd0d
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-11-15 21:22:22 -0800

    Create a script that creates a table called first_table in the current database

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- a script that creates a table called first_table
+-- create the table first_table with the attributes id and name
+CREATE TABLE IF NOT EXISTS first_table(
+       id INT,
+       name VARCHAR(256)
+);
