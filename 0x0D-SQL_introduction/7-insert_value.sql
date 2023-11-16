3cb9c4b3b1a2918649fdc046d72013b6682332de
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-11-15 21:40:45 -0800

    Write a script that adds a row in the table first_table with predetermined values

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- A script that inserts a new row in the table first_table (database hbtn_0c_0)
+-- Insert a row with the values id = 89 and name = "Best School"
+INSERT INTO first_table (id, name) VALUES (89, "Best School")
