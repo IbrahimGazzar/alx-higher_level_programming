144512a73a75fc7eb9c21ea86194b6c70bef810a
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-11-16 04:58:24 -0800

    Fix syntax error

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 -- A script that creates the table unique_id
 -- Create table
-CREATE TABLE IF NOT EXISTS unique_id(id INT DFAULT 1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
