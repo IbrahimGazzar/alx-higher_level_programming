71ce1842eba4e27bc175db94ce030432ef30ac4d
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-12-11 12:21:57 -0800

    Use an external function for factorial calculation

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,16 @@
 #!/usr/bin/node
 
+function factor (num) {
+  if (num <= 1) {
+    return 1;
+  }
+  return num * factor(num - 1);
+}
 const argv = process.argv;
 let num = NaN;
 let fact = 1;
 if (argv[2]) {
   num = parseInt(argv[2]);
-  while (num > 0) {
-    fact *= num;
-    num -= 1;
-  }
+  fact = factor(num);
 }
 console.log(fact);
