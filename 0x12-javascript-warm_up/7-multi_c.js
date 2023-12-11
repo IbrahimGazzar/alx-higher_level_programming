b6b585afe9fb03df39f9d396493de248f6395b49
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-12-11 11:41:53 -0800

    Reduce the number of console.log calls

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
 
 const argv = process.argv;
-let num;
+let num = NaN;
 if (argv[2]) {
   num = parseInt(argv[2]);
   if (num || num === 0) {
     for (let i = 0; i < num; i++) {
       console.log('C is fun');
     }
-  } else {
-    console.log('Missing number of occurrences');
   }
-} else {
+} if (!num) {
   console.log('Missing number of occurrences');
 }
