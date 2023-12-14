2e2b0fd2c928ce0f85f932879e21be138cd918db
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-12-14 12:03:49 -0800

    Search for a specific state

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+
+conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                       passwd=argv[2], db=argv[3], charset="utf8")
+cur = conn.cursor()
+cur.execute(f"""SELECT * FROM states WHERE name LIKE
+             '{argv[4]}' ORDER BY id ASC""")
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
