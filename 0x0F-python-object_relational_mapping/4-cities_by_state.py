7c6f7fa049a72da4f8579678dd8273e635e1e0b6
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-12-14 12:30:18 -0800

    Use python code for a joined query

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+
+conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                       passwd=argv[2], db=argv[3], charset="utf8")
+cur = conn.cursor()
+cur.execute("""SELECT c.id, c.name, s.name
+               FROM states as s, cities as c
+               WHERE c.state_id = s.id
+               ORDER BY c.id ASC""")
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
