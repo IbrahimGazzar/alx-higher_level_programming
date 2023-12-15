f357e929028d073cd9c208a54f85cb0b8173a619
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-12-15 03:53:07 -0800

    .

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,13 +5,14 @@ This script allows the user to search for a specific state
 import MySQLdb
 from sys import argv
 
+
 if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute(f"""SELECT * FROM states
-                    WHERE name LIKE BINARY '{argv[4]}'
-                    ORDER BY id ASC""")
+    cur.execute("""SELECT * FROM states
+                    WHERE name LIKE BINARY '{}'
+                    ORDER BY id ASC""".format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
