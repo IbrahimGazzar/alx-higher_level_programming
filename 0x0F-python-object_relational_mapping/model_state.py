b4ae5cb504c8fe6ea0b4b30b990e7490a8e6fb0b
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-12-15 11:37:52 -0800

    Create base class for state

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module contains the Base and State classes used to hndle
+the database.
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    This class describes a state object in a database
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
