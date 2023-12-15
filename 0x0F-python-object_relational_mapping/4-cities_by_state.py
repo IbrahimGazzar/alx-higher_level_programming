b9b6cefa909e44e3ff1456b93f758ca5e0e8007b
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-12-15 03:58:08 -0800

    .

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
+"""
+This script lists all cities along with the states they are in
+"""
 import MySQLdb
 from sys import argv
 
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                       passwd=argv[2], db=argv[3], charset="utf8")
-cur = conn.cursor()
-cur.execute("""SELECT c.id, c.name, s.name
-               FROM states as s, cities as c
-               WHERE c.state_id = s.id
-               ORDER BY c.id ASC""")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("""SELECT c.id, c.name, s.name
+                   FROM states AS s, cities AS c
+                   WHERE c.state_id = s.id
+                   ORDER BY c.id ASC""")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
