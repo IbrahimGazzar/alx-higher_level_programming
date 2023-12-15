33e8895fa1aaa1caf7df690f371f0a8cbb6d019f
IbrahimGazzar <Ibrahim.m.Elgazzar@gmail.com>
2023-12-15 12:33:01 -0800

    .

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,7 +14,6 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    
     state = session.query(State).order_by(State.id).first()
     if state is None:
         print("Nothing")
